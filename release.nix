49d433ec90d5be1ddba5235d3039107c69f60e91
Michael Bishop <cleverca22@gmail.com>
2020-11-29 08:09:48 -0400

    fix typo

--- release.nix
+++ release.nix
@@ -1,6 +1,6 @@
 let
   self = import ./.;
-{
+in {
   pi400_keyboard = self.pkgsCross.armv7l-hf-multiplatform.pkgsStatic.keyboard.rootZip;
-  bcm2711_msd = pkgsCross.armv7l-hf-multiplatform.pkgsStatic.msd.rootZip;
+  bcm2711_msd = self.pkgsCross.armv7l-hf-multiplatform.pkgsStatic.msd.rootZip;
 }

b5d0353af546aeab8eb98981568dc7b478a10cc6
Michael Bishop <cleverca22@gmail.com>
2021-09-24 02:03:26 -0300

    add valgrind patch for native builds

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     nixpkgs.url = "path:/home/clever/apps/rpi/nixpkgs-test";
   };
   outputs = { self, nixpkgs, utils, rpi-eeprom, rpi-open-firmware, firmware }:
-  utils.lib.eachSystem [ "x86_64-linux" "armv7l-linux" "armv6l-linux" ] (system:
+  utils.lib.eachSystem [ "x86_64-linux" "armv7l-linux" "armv6l-linux" "aarch64-linux" ] (system:
   let
     overlay = self: super: {
       inherit rpi-eeprom firmware;
@@ -28,16 +28,21 @@
       initrd_basic = self.callPackage ./initrd.nix {};
       e2fsprogs = null;
       zstd = null;
+      valgrind = super.valgrind.overrideAttrs (old: {
+        patches = [ ./valgrind.patch ];
+      });
     };
     systemTable = {
       x86_64-linux = "x86_64-linux";
       armv7l-linux = "armv7l-linux";
       armv6l-linux = "armv6l-linux";
+      aarch64-linux = "aarch64-linux";
     };
     crosser = {
       x86_64-linux = x: x;
       armv6l-linux = x: x;
       armv7l-linux = x: x;
+      aarch64-linux = x: x;
       #armv7l-linux = x: x.pkgsCross.armv7l-hf-multiplatform;
       #armv6l-linux = x: x.pkgsCross.raspberryPi;
     };

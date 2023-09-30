618a268c2dae2aba14d97ec05a86375e05bdba6f
Michael Bishop <cleverca22@gmail.com>
2023-09-30 20:00:24 -0300

    allow keys to be missing

--- signing-tool/bootcode.js
+++ signing-tool/bootcode.js
@@ -33,31 +33,36 @@ function parse_bootcode(full_bootcode) {
   const hmac_sig = footer.slice(264, 284);
   console.log("hmac sig:", hmac_sig.toString("hex"));
 
-  var keys = require("./keys");
-  const hmac_payload = full_bootcode.slice(0, full_bootcode.length - 20);
+  try {
+    var keys = require("./keys");
+    const hmac_payload = full_bootcode.slice(0, full_bootcode.length - 20);
 
+    // OTP values 19,20,21,22, in native byte order
+    // this converts them to a 16 byte blob, as it would have been in ram
+    var otp = Buffer.alloc(20,0);
+    for (var i=0; i<keys.otp.length; i++) {
+      otp.writeUInt32LE(keys.otp[i], i*4);
+    }
 
-  // OTP values 19,20,21,22, in native byte order
-  // this converts them to a 16 byte blob, as it would have been in ram
-  var otp = Buffer.alloc(20,0);
-  for (var i=0; i<keys.otp.length; i++) {
-    otp.writeUInt32LE(keys.otp[i], i*4);
+    // found at addresss 0x600035a4 on my maskrom
+    // `dw -b 0x600100f0 20` also reveals it in sram
+    var salt = Buffer.from(keys.salt);
+
+    var key = Buffer.alloc(20, 0);
+    var key = Buffer.alloc(20, 0);
+    for (var i=0; i<key.length; i++) {
+      key[i] = salt[i] ^ otp[i];
+    }
+
+    var hmac = crypto.createHmac("sha1", key);
+    hmac.update(hmac_payload);
+    const actual_hmac_sig = hmac.digest();
+    console.log("expected hmac sig:", actual_hmac_sig.toString("hex"));
+  } catch (e) {
+    return {
+      key_index: key_index
+    };
   }
-
-  // found at addresss 0x600035a4 on my maskrom
-  // `dw -b 0x600100f0 20` also reveals it in sram
-  var salt = Buffer.from(keys.salt);
-
-  var key = Buffer.alloc(20, 0);
-  var key = Buffer.alloc(20, 0);
-  for (var i=0; i<key.length; i++) {
-    key[i] = salt[i] ^ otp[i];
-  }
-
-  var hmac = crypto.createHmac("sha1", key);
-  hmac.update(hmac_payload);
-  const actual_hmac_sig = hmac.digest();
-  console.log("expected hmac sig:", actual_hmac_sig.toString("hex"));
 
   return {
     key_index: key_index,

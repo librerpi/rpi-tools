7d8255172ff134a8a7c2380370271884cb965b5e
Michael Bishop <cleverca22@gmail.com>
2024-02-12 18:14:00 -0400

    improve ramdumper build

--- utils/ramdumper.cc
+++ utils/ramdumper.cc
@@ -1,5 +1,5 @@
 #include <fcntl.h>
-#include <hexdump.h>
+#include "hexdump.h"
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -9,23 +9,25 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <vc_mem.h>
+// #include <vc_mem.h>
 
 int main(int argc, char **argv) {
   int opt;
   char *device = NULL;
   uint32_t addr = 0;
   uint32_t length = 32;
-  bool vcmem = false;
+  //bool vcmem = false;
   while ((opt = getopt(argc, argv, "mvgd:a:l:")) != -1) {
     switch (opt) {
     case 'm':
       device = "/dev/mem";
       break;
+/*
     case 'v':
       device = "/dev/vc-mem";
       vcmem = true;
       break;
+*/
     case 'g':
       device = "/dev/gpiomem";
       break;
@@ -51,6 +53,7 @@ int main(int argc, char **argv) {
     perror("reason");
     return 2;
   }
+  /*
   if (vcmem) {
     uint32_t extra_offset = 0;
     int ret = ioctl(fd, VC_MEM_IOC_MEM_BASE, &extra_offset);
@@ -60,6 +63,7 @@ int main(int argc, char **argv) {
     }
     addr += extra_offset;
   }
+  */
   uint32_t read_offset = addr & 0xfff;
   addr = (addr >> 12) << 12;
   volatile void *rawaddr = (uint32_t*)mmap(NULL, length + read_offset, PROT_READ, MAP_SHARED, fd, addr);
